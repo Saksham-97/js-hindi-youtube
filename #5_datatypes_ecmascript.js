b955b6fdc33aa89aa5be1752f1f394359f940daa
Saksham Singh <officiallysaksham@gmail.com>
2023-08-27 14:52:36 +0530

    datatypes and ecmascript

---
+++ #5_datatypes_ecmascript.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hi"); // we can only use this syntax in browser or via binding js file with html file
+
+// console.log(3+3)
+// console.log("Saksham") // code readability should be high
+
+// primitive datatypes:
+let name = "Saksham" // string datatype
+let age = 3 // number datatype
+let isLoggedIn = true // boolean datatype(true/false)
+let num = 654651464654165415 // bigInt
+let state = null // null datatype, null is datatype also a standalone value
+let x; // undefined datatype
+// symbol datatype is use to define somthing unique
+
+console.log(typeof state)
+
+// object
