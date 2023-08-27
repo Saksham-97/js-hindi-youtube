45f3302ab5b99430a720c65fe6eac979e6e15254
Saksham Singh <officiallysaksham@gmail.com>
2023-08-28 05:08:10 +0530

    #6_dataype_conversion

---
+++ #6_datatype_conversion.js
@@ -0,0 +1,64 @@
+// let score = "33";
+// console.log("score = ", score)
+
+// console.log("type of score = ", typeof score)
+// console.log("type of score = ",typeof (score)) // written as a method
+
+// let newScore = Number(score)
+// console.log("type of Score = ",typeof newScore)
+
+// let neww = "33abc"
+// console.log("neww = ",neww)
+// let newwInNumber = Number(neww)
+// console.log("newwInNumber = ",newwInNumber)
+// console.log("type of newwInNumber = ",typeof newwInNumber)
+
+// let noo = null;
+// console.log("noo = ",noo)
+// let newNoo = Number(noo)
+// console.log("newNoo = ",newNoo)
+// console.log("type of newNoo = ",typeof newNoo)
+
+// let score2 = undefined
+// console.log("score2 = ",score2)
+// let newScore2 = Number(score2)
+// console.log("newScore2 = ",newScore2)
+// console.log("type of newScore2 = ",typeof newScore2)
+
+// let score3 = true
+// console.log("score3 = ",score3)
+// let newScore3 = Number(score3)
+// console.log("newScore3 = ",newScore3)
+// console.log("type of newScore3 = ",typeof newScore3)
+
+// let name = "Saksham"
+// console.log("name = ",name)
+// let newName = Number(name)
+// console.log("newName = ",newName)
+// console.log("type of newName = ",typeof newName)
+
+// "33" => 33
+// "33abc" => NaN (not a number) , but the type is number
+// true => 1; false => 0
+
+
+// let isLoggedIn = 1;
+// console.log("isLoggedIn = ",isLoggedIn)
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log("booleanIsLoggedIn = ",booleanIsLoggedIn)
+// console.log("type of booleanIsLoggedIn = ",typeof booleanIsLoggedIn)
+// // 1 => true; 0 => false
+
+// let isSignIn = "";
+// console.log("isSignIn = ",isSignIn)
+// let booleanIsSignIn = Boolean(isSignIn);
+// console.log("booleanIsSignIn = ",booleanIsSignIn)
+// console.log("type of booleanIsSignIn = ",typeof booleanIsSignIn)
+// "" => false; "value" => true
+
+let someNumber = 33;
+console.log("someNumber = ",someNumber)
+console.log("typeof someNumber = ",typeof someNumber)
+let stringNumber = String(someNumber)
+console.log("stringNumber = ",stringNumber)
+console.log("typeof stringNumber = ",typeof stringNumber)
