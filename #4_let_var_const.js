05e541d8f9928bccf663df818206756d995de951
Saksham Singh <officiallysaksham@gmail.com>
2023-08-27 14:50:36 +0530

    data-types

---
+++ #4_let_var_const.js
@@ -0,0 +1,30 @@
+let score = 4;
+
+// console.log(score + 1);
+
+const account_Id = 144553;
+// account_Id = 2; // not allowed
+// console.log(account_Id)
+
+let account_Email = "your@email.com"
+account_Email = "hc.com"
+console.log(account_Email);
+
+var account_Password = "12345";
+account_Password = 987;
+console.log(account_Password);
+
+//we can also print all varibles with a single line
+
+console.table([account_Email, account_Id]);
+
+/*
+
+preferred not to use var
+
+*/
+
+let account_State;
+console.log("account_State = ", account_State);
+
+account_City = "Delhi"; // this is also not preferred
