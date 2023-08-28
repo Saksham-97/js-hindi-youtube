0d3c13c6c58837842723c500be4458ce98f33230
Saksham Singh <officiallysaksham@gmail.com>
2023-08-28 06:04:26 +0530

    #8_datatypes_comparison.js

---
+++ #8_datatypes_comparison.js
@@ -0,0 +1,45 @@
+// console.log(2 > 1)
+// console.log(2 >= 1)
+// console.log(2 < 1)
+// console.log(2 <= 1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+
+// console.log("2" > 1) // JS automatically converts the "2" in number
+// console.log("02" > 1) // same condition here
+
+// console.log(true != false)
+// console.log(true == false)
+
+//========================*****======================
+
+// console.log(null == 0)
+// console.log(null < 0)
+// console.log(null > 0)
+// console.log(null >= 0)
+// console.log(null <= 0)
+// there is difference between equality operator and comparison operators working
+// converion opeators converts null to number, treating it as zero thats why null > 0 is false and null >= 0 is true
+
+// but undefind will always gives the value false, no matter what the operation is
+// console.log(undefined == 0)
+// console.log(undefined < 0)
+// console.log(undefined > 0)
+// console.log(undefined >= 0)
+// console.log(undefined <= 0)
+
+//========================*****======================
+
+// === this checks not only the value but also the datatype of the variable
+// let a = "1"
+// let b = 1
+// console.log(a == b) // values are equal
+// console.log(a === b) // but the datatypes are not equal, thats why it gives false
+
+//========================*****======================
+
+// let x = "2"
+// let y = 3
+// console.log(x <= y)
+// console.log(x <== y) // <== is not any operator
+
