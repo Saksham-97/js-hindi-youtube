0cbd4f646573d5062f51f7f44aa626519d45439b
Saksham Singh <officiallysaksham@gmail.com>
2023-08-28 07:50:22 +0530

    #9_datatypes_summary.js

---
+++ #9_datatypes_summary.js
@@ -0,0 +1,44 @@
+// JS is a dynaically typed laguage, because we don't need to define the datatype while declaring a variable
+
+// on the basis of how we can store data in the memory and access that data, there are two types of data(types) , officially
+// A. primitive, these are call by value, whenever we use these, we use the copied value not the memory reference
+// B. non-primitive (also known as reference type)
+
+// 7 types of primitive datatypes, these are call by 
+// 1. String
+// 2. Number
+// 3. Boolean
+// 4. Null (empty)
+// 5. undefined //vairbale is declared and variables space is also declared but value is not declared, hence the name undefined
+// 6. symbol // uses to make the value unique
+// 7. BigInt // very large numbers
+
+// Reference datatypes (non-primitive)
+// 1. Array
+// 2. objects
+// 3. functions
+
+// Arrays:
+
+// const heros = ["ironman", "superman", "blackpanther"]
+
+// objects:
+
+// let myObj = {
+//     name: "Saksham",
+//     age: 27,
+// }
+
+// functions:
+
+// const myFunction = function (){
+//     console.log("Hello World")
+// }
+
+// console.log(typeof heros)
+// console.log(typeof myFunction) // it is called object-function
+// console.log(typeof myObj)
+
+// type of arrays: object
+// type of object: object
+// type of function: object-function
