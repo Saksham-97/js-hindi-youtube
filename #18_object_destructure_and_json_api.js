57ece1c1c9677775c366f2ebd66dcf9b88561c46
Saksham Singh <officiallysaksham@gmail.com>
2023-08-29 15:56:23 +0530

    Add files via upload

---
+++ #18_object_destructure_and_json_api.js
@@ -0,0 +1,36 @@
+// destructing of object
+
+// const course = {
+//     courseName: "js in hindi",
+//     price: "999",
+//     courseInructor: "Sammy"
+// }
+
+// course.courseInructor
+
+// we use it so that we define it once and then use it as many times as we want
+// const {courseInructor} = course
+// console.log(courseInructor)
+
+// we can also change the key name
+// const {courseName: name} = course
+// console.log(name)
+
+// aray destructor is rare
+
+// APIs
+
+// this below is json sructure, its nothing but an object, it's key and value both are in b/w ""
+// {
+//     "name": "Saksham",
+//     "courseName": "js in hindi",
+//     "price": "free"
+// }
+
+// somtimes we get APIs in the form of array also
+// [
+//     {}
+//     {}
+//     {}
+// ]
+
