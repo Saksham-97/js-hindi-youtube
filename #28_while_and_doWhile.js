56020c62f33edf202916b84d795ee8174f81d036
Saksham Singh <officiallysaksham@gmail.com>
2023-08-31 22:35:21 +0530

    #28_while_and_doWhile

---
+++ #28_while_and_doWhile.js
@@ -0,0 +1,25 @@
+// while Loop , we need to enter a terminate condition in this
+
+let index = 0
+
+while ( index <= 10 ) {
+    // console.log(`value of index is ${index}`)
+    index+=2
+}
+
+
+let myArray = ["superman", "ironman", "batman", "blackpanther"]
+let i = 0
+while (i < myArray.length){
+    // console.log(`value is ${myArray[i]}`)
+    i++
+}
+
+// do-while Loop , in this first code is executed then the condition will get checked
+
+let score = 1
+do{
+    console.log(`score is ${score}`)
+    score++
+}
+while (score <= 10)
